c602235b06f767e32c4361f37e4b9ba48af595d1
Aman Rangapur <amanrangapur@gmail.com>
2023-01-11 15:36:56 -0600

    preprocessing with chunks is ready for AQA

--- AQA/data_preprocess.py
+++ AQA/data_preprocess.py
@@ -1,9 +1,12 @@
 from chatgpt_wrapper import ChatGPT
+import numpy as np
 import pandas as pd
 import json
 from tqdm import tqdm
+import math
 
-with open("./convertjsoncombined.jsonl") as f:
+
+with open("./jsonl_files/convertjsoncombined.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=435):
         l = json.loads(line)
@@ -15,9 +18,9 @@ with open("./convertjsoncombined.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df0= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-with open("./convertjsoncombined.jsonl") as f:
+with open("./jsonl_files/convertjsoncombined.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -29,10 +32,9 @@ with open("./convertjsoncombined.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][1]["answers"][0]["text"])
 
     df1= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-with open("./convertjsoncombined.jsonl") as f:
+with open("./jsonl_files/convertjsoncombined.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -44,10 +46,9 @@ with open("./convertjsoncombined.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][2]["answers"][0]["text"])
 
     df2= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-with open("./convert_AQA.jsonl") as f:
+with open("./jsonl_files/convert_AQA.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -59,9 +60,9 @@ with open("./convert_AQA.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df3= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-with open("./convert_AQA.jsonl") as f:
+with open("./jsonl_files/convert_AQA.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -73,10 +74,9 @@ with open("./convert_AQA.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][1]["answers"][0]["text"])
 
     df4= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-with open("./convertjson0.jsonl") as f:
+with open("./jsonl_files/convertjson0.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -88,26 +88,9 @@ with open("./convertjson0.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df5= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-# with open("./convertjson1.jsonl") as f:
-#     title, paragraph_id, context, question, answers = [], [], [], [], []
-#     for line in tqdm(f, total=2):
-#         l = json.loads(line)
-#         # print(l)
-#         title.append(l["title"])
-#         # paragraph_id.append(l["paragraphs"])
-#         context.append(l["paragraphs"][0]["context"])
-#         question.append(l["paragraphs"][0]["qas"][0]["question"])
-#         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
-
-#     df6= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-#                       "title", "context", "question", "answers"])
-
-
-
-with open("./convertjson2.jsonl") as f:
+with open("./jsonl_files/convertjson2.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -119,10 +102,9 @@ with open("./convertjson2.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df7= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-with open("./convertjson3.jsonl") as f:
+with open("./jsonl_files/convertjson3.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -134,27 +116,9 @@ with open("./convertjson3.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df8= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-
-# with open("./convertjson4.jsonl") as f:
-#     title, paragraph_id, context, question, answers = [], [], [], [], []
-#     for line in tqdm(f, total=2):
-#         l = json.loads(line)
-#         # print(l)
-#         title.append(l["title"])
-#         # paragraph_id.append(l["paragraphs"])
-#         context.append(l["paragraphs"][0]["context"])
-#         question.append(l["paragraphs"][0]["qas"][0]["question"])
-#         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
-
-#     df9= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-#                       "title", "context", "question", "answers"])
-
-
-
-with open("./convertjson5.jsonl") as f:
+with open("./jsonl_files/convertjson5.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -166,27 +130,9 @@ with open("./convertjson5.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df10= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-
-# with open("./convertjson6.jsonl") as f:
-#     title, paragraph_id, context, question, answers = [], [], [], [], []
-#     for line in tqdm(f, total=2):
-#         l = json.loads(line)
-#         # print(l)
-#         title.append(l["title"])
-#         # paragraph_id.append(l["paragraphs"])
-#         context.append(l["paragraphs"][0]["context"])
-#         question.append(l["paragraphs"][0]["qas"][0]["question"])
-#         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
-
-#     df11= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-#                       "title", "context", "question", "answers"])
-
-
-
-with open("./convertjson7.jsonl") as f:
+with open("./jsonl_files/convertjson7.jsonl") as f:
     title, paragraph_id, context, question, answers = [], [], [], [], []
     for line in tqdm(f, total=2):
         l = json.loads(line)
@@ -198,34 +144,30 @@ with open("./convertjson7.jsonl") as f:
         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
 
     df12= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-                      "title", "context", "question", "answers"])
+                    "title", "context", "question", "answers"])
 
-
-
-
-# with open("./convertjson0.jsonl") as f:
-#     title, paragraph_id, context, question, answers = [], [], [], [], []
-#     for line in tqdm(f, total=2):
-#         l = json.loads(line)
-#         # print(l)
-#         title.append(l["title"])
-#         # paragraph_id.append(l["paragraphs"])
-#         context.append(l["paragraphs"][0]["context"])
-#         question.append(l["paragraphs"][0]["qas"][0]["question"])
-#         answers.append(l["paragraphs"][0]["qas"][0]["answers"][0]["text"])
-
-#     df5= pd.DataFrame(list(zip(title, context, question, answers)), columns=[
-#                       "title", "context", "question", "answers"])
 
 
 df = pd.concat([df0, df1, df2, df3, df4, df5, df7, df8, df10, df10, df12])
 df.drop_duplicates(['question'])
 # print(df.head)
-df.to_csv('file6.csv')
+# df.to_csv('file6.csv')
+
+
+def chunking_dataset(CHUNK):
+    num_chunks = math.ceil(df.shape[0] / CHUNK)
+    df_list = np.array_split(df, num_chunks)
+
+    total = 0
+    for i in range(len(df_list)):
+        total = total + len(df_list[i])
+        df_list[i].to_pickle(f"./AQA/preprocess/{i}.pkl")
+
+    print(f"Total: {total}")
+
+chunking_dataset(CHUNK=50)
 
 
 # bot = ChatGPT()
 # response = bot.ask("What is 6 times 4?")
 # print(response) 
-
-
