b062bc564dfed1718583e2035b0f5c6a276cc73c
Aman Rangapur <amanrangapur@gmail.com>
2023-01-17 09:28:37 -0600

    chatgpt crawler added

--- CoQA/data_preprocess.py
+++ CoQA/data_preprocess.py
@@ -7,7 +7,8 @@ del coqa["version"]
 print(coqa.head())
 
 cols = ["text","question","answer"]
-
+CHUNK = 50
+DATASET = 'CoQA'
 comp_list = []
 for index, row in coqa.iterrows():
     for i in range(len(row["data"]["questions"])):
@@ -17,19 +18,26 @@ for index, row in coqa.iterrows():
         temp_list.append(row["data"]["answers"][i]["input_text"])
         comp_list.append(temp_list)
 df = pd.DataFrame(comp_list, columns=cols) 
+df["query"] = df["text"].astype(str) + " " + df["question"].astype(str)
+df = df.astype(str).drop_duplicates(["query"]).reset_index(drop=True)
+
+# print(df)
+# print(df["response_label"].describe())
+print(df["query"])
 # df.to_csv("CoQA_data.csv", index=False)
 # data = pd.read_csv("CoQA_data.csv")
 # print("Number of question and answers: ", len(data))
 
-def chunking_dataset(CHUNK):
-    num_chunks = math.ceil(df.shape[0] / CHUNK)
-    df_list = np.array_split(df, num_chunks)
+# def chunking_dataset(CHUNK):
+# num_chunks = math.ceil(df.shape[0] / CHUNK)
+# df_list = np.array_split(df, num_chunks)
 
-    total = 0
-    for i in range(len(df_list)):
-        total = total + len(df_list[i])
-        df_list[i].to_pickle(f"./CoQA/preprocess/{i}.pkl")
+# total = 0
+# for i in range(len(df_list)):
+#     total = total + len(df_list[i])
+#     df_list[i].to_pickle(f"./CoQA/preprocess/{i}.pkl")
 
-    print(f"Total: {total}")
+# print(f"Total: {total}")
+df.to_pickle(f"./input_processed/{DATASET}.pkl")
 
-chunking_dataset(CHUNK=50)
+# chunking_dataset(CHUNK=50)
