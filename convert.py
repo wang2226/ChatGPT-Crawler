64047d9c0375da29a099d919708556e7eb68858f
Aman Rangapur <amanrangapur@gmail.com>
2023-02-06 17:30:34 -0600

    Ran NLI on DialFact

--- convert.py
+++ convert.py
@@ -2,21 +2,37 @@ import numpy as np
 import pandas as pd
 import math
 
-data = pd.read_pickle("./CoQA.pkl")
-data_og = pd.read_pickle("./original_df.pkl")
+data = pd.read_csv("./DialFact_original.csv")
+data_og = pd.read_csv("./DialFact_result.csv")
 # print(data.head())
-data_last = data_og.iloc[-1002:]
 
-df_diff = pd.concat([data['input'],data_last['query']]).drop_duplicates(keep=False)
+# print('*'*40)
+# print(data_og.head())
+
+# # data_last = data_og.iloc[-1002:]
+
+df_diff = pd.concat([data_og['input'],data['query']]).drop_duplicates(keep=False)
 print(df_diff)
 
 outputs = []
-for i in data['output']:
+for i in data_og['output']:
     outputs.append(i)
 
-data_last['chatgpt_response'] = outputs
-del data_last['index']
-del data_last['query']
-data_last.to_csv("CoQA_withResponse.csv", index=True)
+
+
+
+data['chatgpt_response'] = outputs
+del data['context_id']
+del data['id']
+del data['query']
+del data['response_label']
+del data['type_label']
+
+
+data.to_csv("DialFact_withResponse.csv", index=True)
+
+
+# context + question + answer
+# context + question + chatgpt response
 
 
