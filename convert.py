6fed22516951a1fc6c2301c788172d64b8ab9184
Aman Rangapur <amanrangapur@gmail.com>
2023-03-14 20:44:47 -0500

    faviq and codah preprocessing

--- convert.py
+++ convert.py
@@ -2,37 +2,30 @@ import numpy as np
 import pandas as pd
 import math
 
-data = pd.read_csv("./DialFact_original.csv")
-data_og = pd.read_csv("./DialFact_result.csv")
-# print(data.head())
+data_og = pd.read_pickle("./FaVIQ_original.pkl")
+data = pd.read_pickle("./FaVIQ.pkl")
+print(data_og.head())
 
+print(len(data_og))
+print(len(data))
 # print('*'*40)
 # print(data_og.head())
 
 # # data_last = data_og.iloc[-1002:]
 
-df_diff = pd.concat([data_og['input'],data['query']]).drop_duplicates(keep=False)
-print(df_diff)
+# df_diff = pd.concat([data_og['query'],data['input']]).drop_duplicates(keep='first')
+# print(df_diff)
 
-outputs = []
-for i in data_og['output']:
-    outputs.append(i)
+# outputs = []
+# for i in data['output']:
+#     str = i[0:1]
+#     outputs.append(str)
+# data['new_output'] = outputs
 
-
-
-
-data['chatgpt_response'] = outputs
-del data['context_id']
-del data['id']
-del data['query']
-del data['response_label']
-del data['type_label']
+df3 = pd.merge(data_og, data, how='inner', left_on='question', right_on='input')
+df3.to_csv("data_new_faviq.csv", index=True)
 
 
-data.to_csv("DialFact_withResponse.csv", index=True)
 
-
-# context + question + answer
-# context + question + chatgpt response
 
 
